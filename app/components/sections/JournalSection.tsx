59f5f8c642c0e1f3b846810a880a1dc54e573147
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-05-20 19:15:49 +0200

    update

--- app/components/sections/JournalSection.tsx
+++ app/components/sections/JournalSection.tsx
@@ -4,10 +4,10 @@ import { LinkPreview } from "@/components/ui/link-preview";
 export default function JournalSection() {
   return (
     <div className="flex flex-col gap-1 text-justify leading-relaxed">
-      <h2 className="text-xl text-gray-500 font-semibold uppercase">Journal</h2>
+      <h2 className="text-xl text-[#FFA84C] font-semibold uppercase">Journal</h2>
       <div className="text-gray-400 text-sm text-justify leading-relaxed flex items-center gap-2">
         <BadgeNew />
-        <span className="text-muted-foreground" text-base>Currently, I'm building</span>
+        <span className="text-muted-foreground text-base" >Currently, I'm building</span>
         <div>
           <LinkPreview
             url="https://noodl.world"
